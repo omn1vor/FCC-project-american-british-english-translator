d4a5ef8c6d784d3b6cac5b65b0dfaa7862738254
Daniel Pimenov <59614467+omn1vor@users.noreply.github.com>
2021-07-31 22:25:03 +0000

    done project

--- components/translator.js
+++ components/translator.js
@@ -3,7 +3,85 @@ const americanToBritishSpelling = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
+const locales = {
+  'american-to-british': {
+    direction: 0,
+    timeRegex: /(\d?\d):(\d\d)/g,
+    timeSubstitution: '$1.$2',
+    titleRegex: '\\b({title})\\.',
+    titleSubstitution: '$1',
+    oneWay: americanOnly
+  },
+  'british-to-american': {
+    direction: 1,
+    timeRegex: /(\d?\d)\.(\d\d)/g,
+    timeSubstitution: '$1:$2',
+    titleRegex: '\\b{title}(?!\\.)\\b',
+    titleSubstitution: '$&.',
+    oneWay: britishOnly
+  }
+};
+
+
+
 class Translator {
+  
+  translate(body, useHighlight=true) {
+    
+    if (!body.hasOwnProperty('text') || !body.hasOwnProperty('locale')) {
+      return { error: 'Required field(s) missing' };
+    }
+
+    if (!body.text) {
+      return { error: 'No text to translate' };
+    }
+
+    if (!locales[body.locale]) {
+      return { error: 'Invalid value for locale field' };
+    }
+
+    let rules = locales[body.locale];
+    let translated = body.text;
+    
+    // timestamps
+    translated = translated.replace(rules.timeRegex, this.addHighlight(rules.timeSubstitution, useHighlight));
+    
+    // titles
+    for (let key in americanToBritishTitles) {
+      let titleFrom = rules.direction === 0 ? key : americanToBritishTitles[key];      
+      titleFrom = titleFrom.replace('.', '');
+
+      let regex = new RegExp(rules.titleRegex.replace('{title}', titleFrom), 'gi');
+      
+      translated = translated.replace(regex, this.addHighlight(rules.titleSubstitution, useHighlight));      
+    }
+
+    // spelling
+    for (let key in americanToBritishSpelling) {
+      let wordFrom = rules.direction === 0 ? key : americanToBritishSpelling[key];
+      let wordTo = rules.direction === 0 ? americanToBritishSpelling[key] : key;
+      
+      translated = translated.replace(new RegExp(`\\b(?<!\\-)${wordFrom}\\b`, 'gi'), this.addHighlight(wordTo, useHighlight));
+    }
+
+    // one-ways
+    for (let key in rules.oneWay) {
+      let wordFrom = key;
+      let wordTo = rules.oneWay[key];
+      
+      translated = translated.replace(new RegExp(`\\b(?<!\\-)${wordFrom}\\b`, 'gi'), this.addHighlight(wordTo, useHighlight));            
+    }
+
+    if (body.text === translated) {
+      translated = 'Everything looks good to me!';
+    }
+
+    return { text: body.text, translation: translated };
+  }
+
+  addHighlight(str, useHighlight=true) {    
+    return useHighlight ? `<span class="highlight">${str}</span>` : str;
+  }
 
 }
 
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,7 +2,67 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Translator = require('../components/translator.js');
+let trans = new Translator;
+
+let usToGb = [
+  ["Mangoes are my favorite fruit.", "Mangoes are my favourite fruit."],
+  ["I ate yogurt for breakfast.", "I ate yoghurt for breakfast."],
+  ["We had a party at my friend's condo.", "We had a party at my friend's flat."],
+  ["Can you toss this in the trashcan for me?", "Can you toss this in the bin for me?"],
+  ["The parking lot was full.", "The car park was full."],
+  ["Like a high tech Rube Goldberg machine.", "Like a high tech Heath Robinson device."],
+  ["To play hooky means to skip class or work.", "To bunk off means to skip class or work."],
+  ["No Mr. Bond, I expect you to die.", "No Mr Bond, I expect you to die."],
+  ["Dr. Grosh will see you now.", "Dr Grosh will see you now."],
+  ["Lunch is at 12:15 today.", "Lunch is at 12.15 today."]
+];
+let gbToUs = [
+  ["We watched the footie match for a while.", "We watched the soccer match for a while."],
+  ["Paracetamol takes up to an hour to work.", "Tylenol takes up to an hour to work."],
+  ["First, caramelise the onions.", "First, caramelize the onions."],
+  ["I spent the bank holiday at the funfair.", "I spent the public holiday at the carnival."],
+  ["I had a bicky then went to the chippy.", "I had a cookie then went to the fish-and-chip shop."],
+  ["I've just got bits and bobs in my bum bag.", "I've just got odds and ends in my fanny pack."],
+  ["The car boot sale at Boxted Airfield was called off.", "The swap meet at Boxted Airfield was called off."],
+  ["Have you met Mrs Kalyani?", "Have you met Mrs. Kalyani?"],
+  ["Prof Joyner of King's College, London.", "Prof. Joyner of King's College, London."],
+  ["Tea time is usually around 4 or 4.30.", "Tea time is usually around 4 or 4:30."]  
+];
+
+let usToGbHighlighted = [
+  ["Mangoes are my favorite fruit.", "Mangoes are my <span class=\"highlight\">favourite</span> fruit."],
+  ["I ate yogurt for breakfast.", "I ate <span class=\"highlight\">yoghurt</span> for breakfast."]
+];
+
+let gbToUsHighlighted = [
+  ["We watched the footie match for a while.", "We watched the <span class=\"highlight\">soccer</span> match for a while."],
+  ["Paracetamol takes up to an hour to work.", "<span class=\"highlight\">Tylenol</span> takes up to an hour to work."]
+];
 
 suite('Unit Tests', () => {
+  
+  usToGb.forEach(i => {
+    test(`Translate ${i[0]} to British English`, () => {
+      assert.equal(trans.translate({ text: i[0], locale: 'american-to-british' }, false).translation, i[1]);
+    });
+  });
+
+  gbToUs.forEach(i => {
+    test(`Translate ${i[0]} to American English`, () => {
+      assert.equal(trans.translate({ text: i[0], locale: 'british-to-american' }, false).translation, i[1]);
+    });
+  });
+
+  usToGbHighlighted.forEach(i => {
+    test(`Highlight translation in ${i[0]}`, () => {
+      assert.equal(trans.translate({ text: i[0], locale: 'american-to-british' }).translation, i[1]);
+    });
+  });
+
+  gbToUsHighlighted.forEach(i => {
+    test(`Highlight translation in ${i[0]}`, () => {
+      assert.equal(trans.translate({ text: i[0], locale: 'british-to-american' }).translation, i[1]);
+    });
+  });
 
 });
